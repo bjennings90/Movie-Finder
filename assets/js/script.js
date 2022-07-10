8c160379f6f542b80a3b649ed152218fa75503ec
Mykaela Saenz <mickycore@gmail.com>
2022-07-10 12:08:08 -0500

    pushing the correct code for the movie elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,27 +18,17 @@
 
 // Have a quality README (with unique name, description, technologies used, screenshot, and link to deployed application).
 
-// API IMDB Title for plot information
+// Variables that link to the HTML documentation
 
-
-
-// Looks up the streaming services for UTELLY
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'a758775ba6mshcc29d7a6d54b17cp1c01a6jsnafa198b33208',
-        'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-    }
-};
-
-
-// Let Variables for the streaming service function
-let searchMovie = document.getElementById('search-input');
-let searchMovieButton = document.getElementById('search-movie-name');
+let moodBtn = document.getElementById('mood-btn');
+let title = document.getElementsByClassName('title');
+let genre = document.getElementsByClassName('genre');
+let plot = document.getElementsByClassName('description');
+let rating = document.getElementsByClassName('rating-number');
 
 //Shows the movie plot and rating information
 function showPlot() {
-let movieDescription = 'https://imdb-api.com/en/API/SearchMovie/k_a62fqwzy/' + searchMovie.value
+let movieDescription = 'https://imdb-api.com/en/API/SearchMovie/k_a62fqwzy/' +
 fetch(movieDescription)
     .then(response => response.json())
     .then(data => console.log(data));
@@ -48,31 +38,63 @@ fetch(movieDescription)
 
 // this will be the main function that will display all the information about the movie
 function displayDescription(id) {
-    'https://imdb-api.com/en/API/Title/k_a62fqwzy/' + id + '/Ratings'
-    response.plot // shows the movie plot, needs to be linked to a variable and HTML
+     'https://imdb-api.com/en/API/Title/k_a62fqwzy/' + id + '/Ratings'
+    plot[1].innerText = response.plot // shows the movie plot, needs to be linked to a variable and HTML
     response.ratings.imDb // shows IMDB's rating of the movie, needs to be linked ot a variable and HTML
+
+}
+
+// creating elements for the Genre movie selection
+function createMovieElement() {
+    // Elements for the first div/ movie poster image 
+    let mainDiv = document.getElementById('poster-column');
+    let colThumbnail = document.createElement("div");
+    let imgElement = document.createElement("span");
+    let moviePoster = document.createElement("img");
+    // Elements for the second div/ movie information
+    let content = document.createElement("div");
+    let movieName = document.createElement("h5");
+    let genreElement = document.createElement("h6");
+    let moviePlot = document.createElement("p");
+    let movieRating = document.createElement("p");
+    
+    // Recreating the movie card HTML format
+    mainDiv.appendChild(colThumbnail);
+    colThumbnail.appendChild(imgElement);
+    imgElement.appendChild(moviePoster);
+    colThumbnail.appendChild(content);
+    content.appendChild(movieName);
+    content.appendChild(genreElement);
+    content.appendChild(moviePlot);
+    content.appendChild(movieRating);
+
+    colThumbnail.setAttribute("class", "col thumbnail");
+    imgElement.setAttribute("class", "img-element");
+    content.setAttribute("class", "content");
+    movieName.setAttribute("class", "title");
+    genreElement.setAttribute("class", "genre");
+    moviePlot.setAttribute("class", "description");
+    movieRating.setAttribute("class", "rating");
+
+    
+    
+}
+
+
+// Create a for loop so that it shows 12 movies per page
+
+
+// resets the page when needed. 
+function clearGrid() {
+    let parent = document.getElementById("poster-column");
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
 
 
-// Utelly streamign services look up. 
-function streamingServiceLookUp() {
-var streamingSite = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=' + searchMovie.value; 
-fetch(streamingSite, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-    console.log (streamingSite);
-}
-
-
-function streamingService(data) {
-    = "Streaming Service:" + data.locations[0].display_name // need to add in a variable name and link it to the HTML
-}
-
-// Search button name, when clicked it should bring up the movie. 
-searchMovieButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    streamingServiceLookUp();
-    showPlot('tt10872600');
+moodBtn.onLoad(() => {
+    showPlot();
+    displayDescription();
 });
