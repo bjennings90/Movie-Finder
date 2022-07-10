af7923929f1658e8fc41c6510759b231248ee9c8
bjennings90 <101131665+bjennings90@users.noreply.github.com>
2022-07-10 11:59:18 -0500

    added append to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,37 +11,83 @@
 // var movie9 = document.getElementById("movie-9");
 // var movie10 = document.getElementById("movie-10");
 // var movie11 = document.getElementById("movie-11");
-var allPosters = document.querySelector("#all-posters");
+var allPosters = document.querySelector("#thumbnail-grid");
 
-    var url = "https://imdb-api.com/en/API/Top250Movies/k_2si393kp";
-                console.log(url);
-                fetch(url)
-                  .then((response) => response.json())
-                  .then((data) => {
-                    // if we see a CORS error, we can’t use that API!
-                    console.log(data);
-                    moviePosters(data)
-                  });
+var url = "https://imdb-api.com/en/API/Top250Movies/k_2si393kp";
+console.log(url);
 
-  function moviePosters(data) {
-    for (var i = 0; i < data.length; i++) {
-        var image = document.createElement("img");
-        var title = document.createElement("h5");
-        var genre = document.createElement("h6");
-        var description = document.createElement("p");
-        var rating = document.createElement("p");
-      image.setAttribute("src",data.items[i].image);  
-      title.textContent = data.items[i].title;
-      genre.textContent = data.items[i].genre;
-      description.textContent = data.items[i].description;
-      rating.textContent = data.items[i].rating;  
 
-      allPosters.append(image);
-    }
+function getMovies() {
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    // if we see a CORS error, we can’t use that API!
+    console.log(data);
+    moviePosters(data);
     
-//   document.querySelector("#zero").setAttribute("src",data.items[i].image);
-//   console.log(data.items[i]);
-  
+  });
+}
+
+function moviePosters(data) {
+  for (var i = 0; i < data.length; i++) {
+    // var image = document.createElement("img");
+    // var title = document.createElement("h5");
+    // var genre = document.createElement("h6");
+    // var description = document.createElement("p");
+    // var rating = document.createElement("p");
+    image.setAttribute("src", data.items[i].image);
+    title.textContent = data.items[i].title;
+    genre.textContent = data.items[i].genre;
+    description.textContent = data.items[i].description;
+    rating.textContent = data.items[i].rating;
+
+    allPosters.append(image);
   }
-  var img = document.querySelector("#zero");
-  console.log(img);
+}
+
+  function createMovieEl() {
+    // Elements for the first div/ movie poster image 
+    let mainDiv = document.getElementById('poster-column');
+    let colThumbnail = document.createElement("div");
+    let imgElement = document.createElement("span");
+    let moviePoster = document.createElement("img");
+    // Elements for the second div/ movie information
+    let content = document.createElement("div");
+    let movieName = document.createElement("h5");
+    let genreElement = document.createElement("h6");
+    let moviePlot = document.createElement("p");
+    let movieRating = document.createElement("p");
+    
+    // Recreating the movie card HTML format
+    mainDiv.appendChild(colThumbnail);
+    colThumbnail.appendChild(imgElement);
+    imgElement.appendChild(moviePoster);
+    colThumbnail.appendChild(content);
+    content.appendChild(movieName);
+    content.appendChild(genreElement);
+    content.appendChild(moviePlot);
+    content.appendChild(movieRating);
+
+    colThumbnail.setAttribute("class", "col thumbnail");
+    imgElement.setAttribute("class", "img-element");
+    content.setAttribute("class", "content");
+    movieName.setAttribute("class", "title");
+    genreElement.setAttribute("class", "genre");
+    moviePlot.setAttribute("class", "description");
+    movieRating.setAttribute("class", "rating");
+  }
+
+  //   document.querySelector("#zero").setAttribute("src",data.items[i].image);
+  //   console.log(data.items[i]);
+
+
+
+var img = document.querySelector("#zero");
+console.log(img);
+
+document.getElementById("mood-btn").addEventListener("click", function () {
+console.log("click")
+createMovieEl();
+getMovies()
+
+});
