5b49f798fcb2ca5834de5b46127e26f27a1fb9c9
Mykaela Saenz <mickycore@gmail.com>
2022-07-10 17:55:05 -0500

    got the genre drop down menu to work and for the images to display on every page

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,34 +19,45 @@
 
 // Have a quality README (with unique name, description, technologies used, screenshot, and link to deployed application).
 
-// Variables that link to the HTML documentation
 
-let moodBtn = document.getElementById('mood-btn');
-let title = document.getElementsByClassName('title');
-let genre = document.getElementsByClassName('genre');
-let plot = document.getElementsByClassName('description');
-let rating = document.getElementsByClassName('rating-number');
+let listener;
+// This is the comedy movie display 
+function displayMovies(genre) {
+    let movieUrl ='https://imdb-api.com/API/AdvancedSearch/k_a62fqwzy?title_type=feature,tv_movie&count=100&genres=' + genre
+    fetch(movieUrl)
+        .then(response => response.json())
+        .then(data => {
+            window.localStorage.setItem('Movies', JSON.stringify(data));
+            displayNextTwelve(0);
+        });
+}
 
-//Shows the movie plot and rating information
-function showPlot() {
-let movieDescription = 'https://imdb-api.com/en/API/SearchMovie/k_a62fqwzy/' +
-fetch(movieDescription)
-    .then(response => response.json())
-    .then(data => console.log(data));
 
-    console.log(movieDescription);
-};
+function displayNextTwelve(start){
+    clearGrid();
+    let dataStorage = window.localStorage.getItem('Movies');
+    let data = JSON.parse(dataStorage);
 
-// this will be the main function that will display all the information about the movie
-function displayDescription(id) {
-     'https://imdb-api.com/en/API/Title/k_a62fqwzy/' + id + '/Ratings'
-    plot[1].innerText = response.plot // shows the movie plot, needs to be linked to a variable and HTML
-    response.ratings.imDb // shows IMDB's rating of the movie, needs to be linked ot a variable and HTML
+    for (let i = start; i < start + 12; i++) {
+        let image = data.results[i].image;
+        let title = data.results[i].title; 
+        let year = data.results[i].description;
+        let genres = data.results[i].genres;
+        let rating = data.results[i].imDbRating;
+        let plot = data.results[i].plot;
 
+        createMovieElement(title, year, genres, rating, plot, image); 
+    }
+    // Alter event listener for display next 12 button with correct offset
+    if (listener != null) {
+        document.getElementById('next-btn').removeEventListener("click", listener);
+    }
+    listener = () => displayNextTwelve(start+12);
+    document.getElementById("next-btn").addEventListener("click", listener);
 }
 
 // creating elements for the Genre movie selection
-function createMovieElement() {
+function createMovieElement(title, year, genres, rating, plot, image) {
     // Elements for the first div/ movie poster image 
     let mainDiv = document.getElementById('poster-column');
     let colThumbnail = document.createElement("div");
@@ -60,7 +71,6 @@ function createMovieElement() {
     let movieRating = document.createElement("p");
     
     // Recreating the movie card HTML format
-    mainDiv.appendChild(colThumbnail);
     colThumbnail.appendChild(imgElement);
     imgElement.appendChild(moviePoster);
     colThumbnail.appendChild(content);
@@ -68,6 +78,7 @@ function createMovieElement() {
     content.appendChild(genreElement);
     content.appendChild(moviePlot);
     content.appendChild(movieRating);
+    mainDiv.appendChild(colThumbnail);
 
     colThumbnail.setAttribute("class", "col thumbnail");
     imgElement.setAttribute("class", "img-element");
@@ -77,14 +88,48 @@ function createMovieElement() {
     moviePlot.setAttribute("class", "description");
     movieRating.setAttribute("class", "rating");
 
-    
+    movieName.textContent = title + " " + year;
+    genreElement.textContent = genres;
+    moviePlot.textContent = plot;
+    movieRating.textContent = "Rating: " + rating + " out of 10";   
+    moviePoster.src = image;
     
 }
 
 
-// Create a for loop so that it shows 12 movies per page
+// This function will show the selected movie genre when the user chooses it from the drop down menu
+function chooseGenre() {
+    let genreType = document.getElementById("select-emoji");
+    let genreChoice = genreType.value;
 
-
+    switch(genreChoice) {
+        case "comedy":
+            displayMovies("comedy");
+            break;
+        case "drama":
+            displayMovies("drama");
+            break;
+        case "romance":
+            displayMovies("romance");
+            break;
+        case "sci-fi":
+            displayMovies("sci_fi");
+            break;
+        case "horror":
+            displayMovies("horror");
+            break;
+        case "action":
+            displayMovies("action");
+            break;
+        case "family":
+            displayMovies("family");
+            break;
+        case "documentaries":
+            displayMovies("documentary");
+            break;
+    }   
+            
+}
 // resets the page when needed. 
 function clearGrid() {
     let parent = document.getElementById("poster-column");
@@ -94,8 +139,4 @@ function clearGrid() {
 }
 
 
-
-moodBtn.onLoad(() => {
-    showPlot();
-    displayDescription();
-});
+document.getElementById('mood-btn').addEventListener("click", () => chooseGenre());
